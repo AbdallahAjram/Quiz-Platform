9bc6cdf40d3c06441bd04f0296cccc9e280cded4
Abdallah Ajram <ajrama_04@outlook.com>
2026-01-10 23:56:46 +0200

    fix(database): add HasFactory trait to models
    This commit adds the HasFactory trait to the Course, Quiz, Question, Enrollment, Lesson, LessonCompletion, and QuizAttempt models. This is necessary to resolve the 'Call to undefined method factory()' error when running the database seeders.

--- app/Models/LessonCompletion.php
+++ app/Models/LessonCompletion.php
@@ -2,11 +2,13 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class LessonCompletion extends Model
 {
+    use HasFactory;
     protected $table = 'lesson_completions';
     protected $primaryKey = 'Id';
 
